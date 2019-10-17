f43ac18c166212eb50a92d901e8209330d2cea3d
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-17 01:12:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,8 +22,8 @@ elsif board[index] ==  "X"||"O"
 end
 end
 
-def input_to_index(index)
-  index.to_i - 1
+def input_to_index(input)
+  index=input.to_i - 1
 end
 
 def move(board, index, current_player="X")
