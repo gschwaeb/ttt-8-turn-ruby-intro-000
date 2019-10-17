9642c4e2fea935a9894d2e0e08bb0beb995599b7
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-17 00:29:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,24 @@ puts" #{board[0]} | #{board[1]} | #{board[2]} "
 puts"-----------"
  puts" #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def valid_move?(board, index)
+   if (index.between?(0, 8)) && (position_taken?(board, index) == false)
+ return true
+else
+  return false
+end
+end
+
+def position_taken? (board, index)
+if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  return false
+elsif board[index] ==  "X"||"O"
+  return true
+end
+end
+def input_to_index(input)
+  input.to_i - 1
+end
+def move(board,index,current_player="X")
+  board[index]=current_player
+end
